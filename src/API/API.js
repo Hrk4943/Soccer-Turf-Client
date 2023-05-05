73c0bed60a64505849b131341016cd5ff395389b
Hrithik Gopi <hrithikgopi5@gmail.com>
2023-05-05 10:14:23 +0530

    normal api comment

--- src/API/API.js
+++ src/API/API.js
@@ -1,5 +1,5 @@
-import axios from "axios"
+// import axios from "axios"
 
-export const userUrl='http://localhost:9000/'
-export const turfOwnerUrl='http://localhost:9000/turfOwner/'
-export const adminUrl='http://localhost:9000/admin/'
+// export const userUrl='http://localhost:9000/'
+// export const turfOwnerUrl='http://localhost:9000/turfOwner/'
+// export const adminUrl='http://localhost:9000/admin/'
