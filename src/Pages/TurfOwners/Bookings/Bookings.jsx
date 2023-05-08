93b752bf18f4e556adb05739014100c37746fd16
Hrithik Gopi <hrithikgopi5@gmail.com>
2023-05-08 15:31:28 +0530

    turf booking search

--- src/Pages/TurfOwners/Bookings/Bookings.jsx
+++ src/Pages/TurfOwners/Bookings/Bookings.jsx
@@ -13,7 +13,7 @@ export default function Bookings() {
     const [upcomingBookings, setUpcomingBookings] = useState([]);
     const [previousBookings, setPreviousBookings] = useState([]);
     const [showBookings, setShowBookings] = useState(true);
-
+      const [searchDate, setSearchDate] = useState("");
     let today = new Date();
     let month = String(today.getMonth() + 1).padStart(2, "0");
     let day = String(today.getDate()).padStart(2, "0");
@@ -106,6 +106,19 @@ export default function Bookings() {
         fetchBookings(token);
     }, []);
 
+    const searchBookings = (e) => {
+        e.preventDefault();
+        const filteredBookings = bookings.filter((booking) => {
+            const bookedDate = new Date(booking.bookDate);
+            return (
+                bookedDate.getTime() >= new Date(searchDate).getTime() &&
+                bookedDate.getTime() <= new Date(searchDate).getTime() + 86400000
+            ); //86400000 is the number of milliseconds in a day
+        });
+        setUpcomingBookings(filteredBookings);
+        setPreviousBookings([]);
+    };
+
 
     return (
         <>
@@ -116,6 +129,16 @@ export default function Bookings() {
                     <div>
                         <h2 class="text-2xl font-semibold leading-tight text-center">Bookings</h2>
                     </div>
+                    <form onSubmit={searchBookings}>
+                        <label htmlFor="searchDate">Search by date:</label>
+                        <input
+                            type="date"
+                            id="searchDate"
+                            value={searchDate}
+                            onChange={(e) => setSearchDate(e.target.value)}
+                        />
+                        <button type="submit"> Search</button>
+                    </form>
                     <table className=" table w-full border-2 border-slate-950">
                         <thead>
                             <tr>
@@ -167,9 +190,9 @@ export default function Bookings() {
                                         </td> */}
                                             <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                                 <p class="text-gray-900 whitespace-no-wrap">
-                                                {moment(booking?.bookDate).format('DD-MM-YYYY')}
+                                                    {moment(booking?.bookDate).format('DD-MM-YYYY')}
                                                     {/* {new Date(booking?.bookDate).toLocaleDateString()} */}
-                                                    </p>
+                                                </p>
                                             </td>
                                             <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                                 <p class="text-gray-900 whitespace-no-wrap">{booking?.time}</p>
@@ -201,10 +224,10 @@ export default function Bookings() {
                                             </td>
                                             <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                                 <p class="text-gray-900 whitespace-no-wrap">
-                                                    
-                                                {moment(booking?.bookDate).format('DD-MM-YYYY')}
-                                                {/* {new Date(booking?.bookDate).toLocaleDateString()} */}
-                                                    </p>
+
+                                                    {moment(booking?.bookDate).format('DD-MM-YYYY')}
+                                                    {/* {new Date(booking?.bookDate).toLocaleDateString()} */}
+                                                </p>
                                             </td>
                                             <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                                 <p class="text-gray-900 whitespace-no-wrap">{booking?.time}</p>
@@ -239,7 +262,7 @@ export default function Bookings() {
                             className="btn btn-border-2 float-right mt-5"
                             onClick={() => setShowBookings(true)}
                         >
-                           Current Bookings 
+                            Current Bookings
                         </button>
                     )}
                 </div>
